7c55000440e6f6e2e94328c9280c0fc144a05664
Hicham113 <ameur_hicham@hotmail.com>
2016-03-19 21:35:03 +0000

    Initial commit

---
+++ app/src/main/java/com/deitel/hicham/listviewdemo/app/ListViewDemo.java
@@ -0,0 +1,16 @@
+package com.deitel.hicham.listviewdemo.app;
+
+/**
+ * Created by Hicham on 18/03/2016.
+ */
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ListViewDemo extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.listview);
+    }
+}
+
---
+++ app/src/main/java/com/deitel/hicham/listviewdemo/app/MainActivity.java
@@ -0,0 +1,26 @@
+package com.deitel.hicham.listviewdemo.app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String[]  myStringArray={"A","B","C"};
+        ArrayAdapter<String> myAdapter=new
+                ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_1,
+                myStringArray);
+        ListView myList=(ListView)
+                findViewById(R.id.litViewlayout);
+        myList.setAdapter(myAdapter);
+    }
+    }
+
